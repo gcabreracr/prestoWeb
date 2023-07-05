4a70a2bca0ef1051d5295691e8ac2744d78d9017
gcabreracr <gcabreracr@gmail.com>
2023-07-04 23:23:18 -0600

    PC 4jul 11pm

--- js/crearRecibo.js
+++ js/crearRecibo.js
@@ -11,14 +11,14 @@ function creaTkt57mm(datos) {
     const ANCHO = 25;
 
     let titTkt = sessionStorage.getItem('NOM_CIA');
-    let _titTkt = titTkt.length + Math.trunc((ANCHO - titTkt.length) / 2);  
+    let _titTkt = titTkt.length + Math.trunc((ANCHO - titTkt.length) / 2);
 
     let mNumRec = datos.num_recibo;
     let linNumRec = 'Recibo Dinero # ' + mNumRec;
     let _linNumRec = linNumRec.length + Math.trunc((ANCHO - linNumRec.length) / 2);
 
-    
-  
+
+
     let mFechaRec = datos.fec_recibo;
     let nomCliente = datos.nomCliente;
     let detAbono = datos.det_abono;
@@ -29,26 +29,27 @@ function creaTkt57mm(datos) {
 
     let mUsuario = sessionStorage.getItem('COD_USUARIO');
     let linFirma = 'Emitido por ' + mUsuario;
-    let _linFirma = linFirma.length + Math.trunc((ANCHO - linFirma.length) / 2);   
+    let _linFirma = linFirma.length + Math.trunc((ANCHO - linFirma.length) / 2);
 
     let tkt = '<pre>';
-    tkt += titTkt.padStart(_titTkt) + LF;
+    tkt += '<strong>' + titTkt.padStart(_titTkt) + '</strong>' + LF;
     tkt += linNumRec.padStart(_linNumRec) + LF + LF;
-    tkt += 'Fecha'.padEnd(8) + mFechaRec + LF;
-    tkt += 'Hemos recibido de: '+ nomCliente + LF;
+    tkt += 'Fecha'.padEnd(8) + mFechaRec + LF + LF;
+    tkt += 'Hemos recibido de: ' + LF;
+    tkt += nomCliente + LF + LF;
     //tkt += nomCliente + LF + LF;
-    tkt += 'La suma de :' +   mMonAbo.padStart(10) + ' colones' + LF;
+    tkt += 'La suma de :' + mMonAbo.padStart(10) + ' colones' + LF + LF;
 
-    tkt += 'Por concepto de :' + detAbono + LF + LF;
-    
-   //tkt += 'Saldo Anterior'.padEnd(15,'.') +  FNT.format(mSalIni).padStart(10) + LF;
-   tkt += 'Saldo Anterior'.padEnd(15,'.') +  mSalIni.padStart(10) + LF;
+    tkt += 'Por concepto de: ' + detAbono + LF + LF;
 
-   //tkt += 'Este Abono'.padEnd(15,'.') +  FNT.format(mMonAbo).padStart(10) + LF;
-   tkt += 'Este Abono'.padEnd(15,'.') +  mMonAbo.padStart(10) + LF;
+    //tkt += 'Saldo Anterior'.padEnd(15,'.') +  FNT.format(mSalIni).padStart(10) + LF;
+    tkt += 'Saldo Anterior'.padEnd(15, '.') + mSalIni.padStart(10) + LF;
 
-   //tkt += 'Nuevo Saldo'.padEnd(15,'.') +  FNT.format(mSalFin).padStart(10) + LF + LF + LF;
-   tkt += 'Nuevo Saldo'.padEnd(15,'.') +  mSalFin.padStart(10) + LF + LF + LF;
+    //tkt += 'Este Abono'.padEnd(15,'.') +  FNT.format(mMonAbo).padStart(10) + LF;
+    tkt += 'Este Abono'.padEnd(15, '.') + mMonAbo.padStart(10) + LF;
+
+    //tkt += 'Nuevo Saldo'.padEnd(15,'.') +  FNT.format(mSalFin).padStart(10) + LF + LF + LF;
+    tkt += 'Nuevo Saldo'.padEnd(15, '.') + mSalFin.padStart(10) + LF + LF + LF;
 
     tkt += linFirma + LF + LF + '</pre>';
 
@@ -62,7 +63,7 @@ function creaTkt80mm(datos) {
     const ANCHO = 28;
 
     let titTkt = sessionStorage.getItem('NOM_CIA');
-    let _titTkt = titTkt.length + Math.trunc((ANCHO - titTkt.length) / 2);  
+    let _titTkt = titTkt.length + Math.trunc((ANCHO - titTkt.length) / 2);
 
     let mNumRec = datos.num_recibo;
     let linNumRec = 'Recibo Dinero # ' + mNumRec;
@@ -78,26 +79,27 @@ function creaTkt80mm(datos) {
 
     let mUsuario = sessionStorage.getItem('COD_USUARIO');
     let linFirma = 'Emitido por ' + mUsuario;
-    let _linFirma = linFirma.length + Math.trunc((ANCHO - linFirma.length) / 2);    
+    let _linFirma = linFirma.length + Math.trunc((ANCHO - linFirma.length) / 2);
 
     let tkt = '<pre>';
-    tkt += titTkt.padStart(_titTkt) + LF;
+    tkt += '<strong>' + titTkt.padStart(_titTkt) + '</strong>' + LF;
     tkt += linNumRec.padStart(_linNumRec) + LF + LF;
-    tkt += 'Fecha'.padEnd(8) + mFechaRec + LF;
-    tkt += 'Hemos recibido de: '+ nomCliente + LF;
+    tkt += 'Fecha'.padEnd(8) + mFechaRec + LF + LF;
+    tkt += 'Hemos recibido de: ' + LF;
+    tkt += '<strong>' + nomCliente + '</strong>' + LF + LF;
     //tkt += nomCliente + LF + LF;
-    tkt += 'La suma de :' +   mMonAbo.padStart(10) + ' colones' + LF;
+    tkt += 'La suma de :' + '<strong>' + mMonAbo.padStart(10) + ' colones</strong>' + LF + LF;
 
-    tkt += 'Por concepto de :' + detAbono + LF + LF;
+    tkt += 'Por concepto de: ' + detAbono + LF + LF;
 
     //tkt += 'Saldo Anterior'.padEnd(15,'.') +  FNT.format(mSalIni).padStart(10) + LF;
-    tkt += 'Saldo Anterior'.padEnd(15,'.') +  mSalIni.padStart(10) + LF;
+    tkt += 'Saldo Anterior'.padEnd(15, '.') + mSalIni.padStart(10) + LF;
 
     //tkt += 'Este Abono'.padEnd(15,'.') +  FNT.format(mMonAbo).padStart(10) + LF;
-    tkt += 'Este Abono'.padEnd(15,'.') +  mMonAbo.padStart(10) + LF;
+    tkt += 'Este Abono'.padEnd(15, '.') + mMonAbo.padStart(10) + LF;
 
     //tkt += 'Nuevo Saldo'.padEnd(15,'.') +  FNT.format(mSalFin).padStart(10) + LF + LF + LF;
-    tkt += 'Nuevo Saldo'.padEnd(15,'.') +  mSalFin.padStart(10) + LF + LF + LF;
+    tkt += 'Nuevo Saldo'.padEnd(15, '.') + mSalFin.padStart(10) + LF + LF + LF;
 
     tkt += linFirma + LF + LF + '</pre>';
 
@@ -106,24 +108,4 @@ function creaTkt80mm(datos) {
 
 }
 
-function imprimeTkt() {
-
-    var divToPrint = document.getElementById("divTT");
-
-    newWin = window.open("");
-    newWin.document.write(divToPrint.outerHTML);
-    newWin.print();
-    newWin.close();
-
-}
-
-function copiaTkt() {
-    let range = document.createRange();
-    range.selectNode(document.getElementById('divTT'));
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand("copy");
-    //alert('Tiquete copiado');
-
-}
 
