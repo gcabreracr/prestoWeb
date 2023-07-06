d11ef88495d8caedc387e25a8e6884576135fb1c
Gonzalo Cabrera <gcabreracr@gmail.com>
2023-07-06 12:34:00 -0600

    PCRV 6jul 12md

---
+++ js/reportesPDF.js
@@ -0,0 +1,33 @@
+
+
+class MovFondo extends jsPDF {
+
+   
+
+
+    constructor(){
+        super();
+
+        this.LTH = 50;
+
+        
+    }
+
+
+    addHeader() {
+
+
+
+    }
+
+    addFootter() {
+
+
+    }
+
+    generaReporte(){
+
+    
+    }
+
+}
